8bec78e5146115d0e55a992a240e346d24513f63
ethanzohar <ethanzohar9@gmail.com>
2019-09-24 18:27:32 -0400

    font please

--- html/watch.php
+++ html/watch.php
@@ -15,6 +15,16 @@ $current_page = "watch";
   <title>Watch</title>
 
   <link rel="stylesheet" href="css/main.css">
+
+  <style>
+@font-face {
+    font-family: 'twitch font';
+    src: url('dimis___-webfont.woff2') format('woff2'),
+         url('dimis___-webfont.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+</style>
 </head>
 
 <body style="background-color: #19171c">
